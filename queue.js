df81977c3d39e2711d27f7933a81e6a704da8216
Rissberger <130178293+Rissberger@users.noreply.github.com>
2024-01-25 11:22:16 -0800

    Add files via upload

---
+++ queue.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) throw new Error("Queue is empty");
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+
+  peek() {
+    if (!this.first) return null;
+    return this.first.value;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+module.exports = Queue;
---
+++ stack.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) throw new Error("Stack is empty");
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+
+  peek() {
+    if (!this.first) return null;
+    return this.first.value;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+module.exports = Stack;
